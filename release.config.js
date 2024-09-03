6f481bc5db12c6db0a1c758794f3c53759bd796e
gavilanbe <nahuel@gavilanbe.io>
2024-09-03 19:29:50 +0200

    feat(config): add commitizen, pre-commit hooks, and semantic-release setup

---
+++ release.config.js
@@ -0,0 +1,11 @@
+// release.config.js
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/github'
+  ]
+};
